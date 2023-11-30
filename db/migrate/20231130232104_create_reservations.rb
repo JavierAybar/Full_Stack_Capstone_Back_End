de43026915891a8d67824e166ea67240c8702bde
Marc raphael <marcraphael979@gmail.com>
2023-12-01 00:21:35 +0100

    Generate reservation

---
+++ db/migrate/20231130232104_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :reservations do |t|
+      t.date :date
+      t.string :city
+      t.string :description
+      t.references :user, null: false, foreign_key: true
+      t.references :vehicle, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
