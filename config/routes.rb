41360412f11be81081b60d077408398ed0a90d47
Marc raphael <marcraphael979@gmail.com>
2023-12-01 00:34:52 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:show, :create, :destroy] do
+        resources :vehicles, only: [:create, :show, :index, :destroy]
+        resources :reservations, only: [:create, :show, :index]
+      end
+    end
+  end
 end
