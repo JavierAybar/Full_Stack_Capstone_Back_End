ef6cdd35e654f4f44f5f94b8003d22be46b70ee1
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-12-01 19:39:55 -0300

    Add list all vehicles

--- app/controllers/api/V1/vehicles_controller.rb
+++ app/controllers/api/V1/vehicles_controller.rb
@@ -4,7 +4,15 @@ class Api::V1::VehiclesController < ApplicationController
 
   def show; end
 
-  def index; end
+  def index
+    @vehicles = Vehicle.order(created_at: :desc).all
+
+    if @vehicles.empty?
+      render json: { error: 'No vehicles found' }, status: :not_found
+    else
+      render json: @vehicles
+    end  
+  end
 
   def destroy; end
 end
