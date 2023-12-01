5befb5b76411b9347c3b13483b11b69ede12fc21
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-11-30 22:44:58 -0300

    Fix linters

--- app/controllers/api/V1/users_controller.rb
+++ app/controllers/api/V1/users_controller.rb
@@ -1,11 +1,8 @@
 class Api::V1::UsersController < ApplicationController
-	# TODO: update actions
-  def create
-  end
+  # TODO: update actions
+  def create; end
 
-  def show
-  end
+  def show; end
 
-  def destroy
-  end
+  def destroy; end
 end
--- app/controllers/api/V1/vehicles_controller.rb
+++ app/controllers/api/V1/vehicles_controller.rb
@@ -1,14 +1,10 @@
 class Api::V1::VehiclesController < ApplicationController
-	# TODO: update actions
-  def create
-  end
+  # TODO: update actions
+  def create; end
 
-  def show
-  end
+  def show; end
 
-	def index
-  end
+  def index; end
 
-  def destroy
-  end
+  def destroy; end
 end
