45d5327b7ccf655e157debb7d6eb448adba143b0
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-12-02 17:53:23 +0200

    Refactor users_controller.rb to include actions

--- app/controllers/api/V1/users_controller.rb
+++ app/controllers/api/V1/users_controller.rb
@@ -1,8 +1,50 @@
 class Api::V1::UsersController < ApplicationController
-  # TODO: update actions
-  def create; end
+  before_action :set_user, only: %i[show update destroy]
 
-  def show; end
+  def index
+    @user = User.all
+    if @user.present?
+      render json: { success: true, users: @user }
+    else
+      render json: { success: false, message: "Users doesn't exists" }
+    end
+  rescue StandardError => e
+    render json: { success: false, message: e.message }
+  end
 
-  def destroy; end
+  def show
+    render json: @user
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: { success: true, user: @user }
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
