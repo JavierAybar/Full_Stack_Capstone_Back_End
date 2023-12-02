dfda41d6014452d043fb1ea141db8f718f03a07c
Marc raphael <marcraphael979@gmail.com>
2023-12-02 02:13:21 +0100

    Add reservation actions
    - index
    - create
    - show
    - destroy

--- app/controllers/api/V1/reservations_controller.rb
+++ app/controllers/api/V1/reservations_controller.rb
@@ -1,10 +1,44 @@
 class Api::V1::ReservationsController < ApplicationController
   # TODO: update actions
-  def create; end
+  def create
+    reservation = Reservation.new(reservations_params)
+    if reservation.save
+      render json: {reservation: reservation}
+    else
+      render json: {errors: reservation.errors.full_messages}, status: :unprocessable_error
+    end
+  end
 
-  def show; end
+  def show
+    reservation = Reservation.find_by(id: params[:id])
 
-  def index; end
+    if reservation
+      render json: {reservation: reservation}
+    else
+      render json: {message: 'not found'}
+    end
+  end
 
-  def destroy; end
+  def index
+    # TODO: show all the reservations of the current authenticated user
+    # reservations = Reservation.where(user_id: @current_user)
+    reservations = Reservation.all
+
+    if reservations
+      render json: {reservations: reservations}
+    else
+      render json: {error: "no reservation yet"}
+    end
+  end
+
+  def destroy
+    reservation = Reservation.find(params[:id]).destroy
+    render json: {destroy: reservation}
+  end
+
+  private
+
+  def reservations_params
+    params.require(:reservation).permit(:date, :city, :user_id, :vehicle_id)
+  end
 end
