084badb8aa838aaa61f918b25201d8a5c9e5d0e2
Marc raphael <marcraphael979@gmail.com>
2023-12-02 02:50:39 +0100

    Fix conflict with vehicles on dev

--- app/controllers/api/V1/vehicles_controller.rb
+++ app/controllers/api/V1/vehicles_controller.rb
@@ -1,34 +1,14 @@
 class Api::V1::VehiclesController < ApplicationController
   # TODO: update actions
-  def create
-    vehicle = Vehicle.new(vehicles_params)
-    if vehicle.save
-      render json: { vehicle: }
-    else
-      render json: { errors: vehicle.errors.full_messages }, status: :unprocessable_error
-    end
-  end
+  def create; end
 
+  def show; end
   def show
-    vehicle = Vehicle.find_by(id: params[:id])
-
-    if vehicle
-      render json: { vehicle: }
-    else
-      render json: { message: 'not found' }
-    end
+    @vehicle = Vehicle.find(params[:id])
+    render json: @vehicle, status: :ok
   end
 
-  def index
-    vehicles = Vehicle.all
-    render json: { vehicles: }
-  end
+  def index; end
 
   def destroy; end
-
-  private
-
-  def vehicles_params
-    params.require(:vehicle).permit(:name, :description, :image)
-  end
 end
