1e60e9d8387f504318f8a297667da10ad781f726
Marc raphael <marcraphael979@gmail.com>
2023-12-02 02:40:20 +0100

    Create template for users and vehicles

--- app/controllers/api/V1/vehicles_controller.rb
+++ app/controllers/api/V1/vehicles_controller.rb
@@ -1,10 +1,34 @@
 class Api::V1::VehiclesController < ApplicationController
   # TODO: update actions
-  def create; end
+  def create
+    vehicle = Vehicle.new(vehicles_params)
+    if vehicle.save
+      render json: { vehicle: }
+    else
+      render json: { errors: vehicle.errors.full_messages }, status: :unprocessable_error
+    end
+  end
 
-  def show; end
+  def show
+    vehicle = Vehicle.find_by(id: params[:id])
 
-  def index; end
+    if vehicle
+      render json: { vehicle: }
+    else
+      render json: { message: 'not found' }
+    end
+  end
+
+  def index
+    vehicles = Vehicle.all
+    render json: { vehicles: }
+  end
 
   def destroy; end
+
+  private
+
+  def vehicles_params
+    params.require(:vehicle).permit(:name, :description, :image)
+  end
 end
