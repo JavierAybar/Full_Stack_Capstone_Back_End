9bf2e18e21e63d562acfb82baca8a5868665aae6
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-12-02 18:12:42 +0200

    Fix linters

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Users::SessionsController < Devise::SessionsController
   include RackSessionFix
   respond_to :json
@@ -30,7 +28,7 @@ class Users::SessionsController < Devise::SessionsController
 
   def respond_with(resource, _opts = {})
     render json: {
-      status: {code: 200, message: 'Logged in sucessfully.'},
+      status: { code: 200, message: 'Logged in sucessfully.' },
       data: UserSerializer.new(resource).serializable_hash[:data][:attributes]
     }, status: :ok
   end
@@ -39,7 +37,7 @@ class Users::SessionsController < Devise::SessionsController
     if current_user
       render json: {
         status: 200,
-        message: "logged out successfully"
+        message: 'logged out successfully'
       }, status: :ok
     else
       render json: {
