5d9e3b3affa5e4db3f320d3c36426fe63ab967e3
Nurbol <nurbol.sultanov@gmail.com>
2023-12-02 04:31:09 -0800

    Add auth controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  protected
+
+  def authenticate_admin!
+    unless current_user&.admin?
+      render json: { error: 'Not authorized' }, status: :unauthorized
+    end
+  end
 end
