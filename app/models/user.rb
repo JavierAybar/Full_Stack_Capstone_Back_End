0d49d926a7ed93f890683aeebb74ab216ffee955
Abdelaziz Mohamed Ali <abdelaziz.dv@gmail.com>
2023-12-02 11:59:04 +0200

    Add JWT authentication to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
   has_many :reservations
+
+  def jwt_payload
+    super.merge('foo' => 'bar')
+  end
 end
